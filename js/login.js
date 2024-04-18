8be8aa810b091c2e1036951007ce2f7135bfb556
nohpedro <147351155+nohpedro@users.noreply.github.com>
2024-04-18 19:35:08 -0400

    Login y register responsivo
    linea 92 mauri!

--- js/login.js
+++ js/login.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('loginForm');
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("loginForm");
 
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        const formData = new FormData(form);
-        
-        fetch('../php/login.php', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => response.text())
-        .then(data => {
-            alert(data);  // Muestra un mensaje de bienvenida o error
-        })
-        .catch(error => console.error('Error:', error));
-    });
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    fetch("../php/login.php", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        alert(data);
+      })
+      .catch((error) => console.error("Error:", error));
+  });
 });
