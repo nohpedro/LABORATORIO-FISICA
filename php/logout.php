02bc36c975b8cfdd725ac516717df70347d55920
nohpedro <147351155+nohpedro@users.noreply.github.com>
2024-04-19 09:17:28 -0400

    Cerrar sesion
    Cierra session la pagina web

--- php/logout.php
+++ php/logout.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-$_SESSION = array(); 
-session_destroy(); 
-header("Location: login.html"); 
-exit;
-?>
+session_unset();  // Remover todas las variables de sesión
+session_destroy(); // Destruir la sesión activa
+
+// Puedes optar por enviar una respuesta que JavaScript pueda leer o simplemente redirigir
+echo json_encode(['status' => 'success', 'message' => 'Session ended']);
+
