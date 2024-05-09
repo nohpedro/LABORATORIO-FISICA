0754ef27cb503edcae1700153007c2274f5f2509
Mauricio <alan.fernandez@ucb.edu.bo>
2024-05-09 19:36:59 -0400

    prueba js

--- Server/app/static/js/include_navbar.js
+++ Server/app/static/js/include_navbar.js
@@ -1,6 +1,6 @@
 // This script loads the navbar from an HTML file and inserts it into the page.
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("navbary.html")
+  fetch("/navbar.html")
     .then((response) => response.text())
     .then((data) => {
       document.body.insertAdjacentHTML("afterbegin", data);
@@ -9,18 +9,24 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-const requestBody = {
-  email: 'admin@example.com',
-  password: '#123#AndresHinojosa#123'
-};
 
-// Make the POST request
+
+var token = ''
+
+var res = ''
+const requestBody = {
+    email: 'admin@example.com',
+    password: '#123#AndresHinojosa#123'
+  };
+
+  
+
 fetch('http://127.0.0.1:8000/api/user/token/', {
   method: 'POST',
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
   },
-  body: JSON.stringify(requestBody)
+  body: JSON.stringify(requestBody),
 })
 .then(response => {
   if (!response.ok) {
@@ -30,8 +36,44 @@ fetch('http://127.0.0.1:8000/api/user/token/', {
 })
 .then(data => {
   // Do something with the JSON response data
-  console.log(data);
+   token = data['token'];
+   console.log(token);
 })
 .catch(error => {
   console.error('There was a problem with the fetch operation:', error);
-});
+}).then(
+
+    
+
+  res = "",
+
+  fetch('http://127.0.0.1:8000/api/user/list/', {
+  method: 'GET',
+  headers: {
+      'Content-Type': 'application/json',
+      'accept': 'application/json',
+      'Authorization' : 'Token ' + token,
+  },
+  })
+  .then(response => {
+  if (!response.ok) {
+      throw new Error('Network response was not ok');
+  }
+  return response.json(); // Parse response body as JSON
+  })
+  .then(data => {
+  // Do something with the JSON response data
+  console.log(data.stringify());
+  })
+  .catch(error => {
+  console.error('There was a problem with the fetch operation:', error);
+  }),
+
+
+
+)
+
+
+
+
+// Make the POST request
